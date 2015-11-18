7f69831da0a1e6cfe1daeeb76527b666645f194e
Joni Lee <joni.kartorz.lee@gmail.com>
2015-11-18 13:50:39 +0800

    Fixed: Delete 'tabBarAutoHide' introduced in Qt 5.4.

--- src/gui/qt/ui_mainwindow.h
+++ src/gui/qt/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.5.1
+** Created by: Qt User Interface Compiler version 5.2.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -349,7 +349,6 @@ public:
         settingTabWidget->setElideMode(Qt::ElideNone);
         settingTabWidget->setDocumentMode(true);
         settingTabWidget->setTabsClosable(false);
-        settingTabWidget->setTabBarAutoHide(false);
         tab = new QWidget();
         tab->setObjectName(QStringLiteral("tab"));
         tab->setStyleSheet(QLatin1String("#tab { \n"
